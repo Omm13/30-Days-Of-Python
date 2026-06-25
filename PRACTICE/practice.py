287adbbd8c6d7fe6fdc36a922bb4a4c5eceeec8e
Omm13 <miriyalaomm@gmail.com>
2026-06-25 23:30:17 +0530

    new: Created new file for more practice

---
+++ PRACTICE/practice.py
@@ -0,0 +1,22 @@
+# DAY-1 practice-1
+
+# # 1. Program to Print Integer Numbers Entered by the User:
+# n = int(input("Enter a number: "))
+# print("The number you entered is:", n)
+
+# # 2.Write a Program to Find the Size of int, float, double, and char on Your Computer: 
+# import sys
+# print("Size of int:", sys.getsizeof(int()))
+# print("Size of float:", sys.getsizeof(float()))
+# print("Size of double:", sys.getsizeof(float()))  
+# print("Size of char:", sys.getsizeof(str()))
+
+# # 3. Program to Find the Larger Number Among Two Numbers:
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# if a > b:
+#     print("The larger number is:", a)
+# elif b > a:
+#     print("The larger number is:", b)
+# else:
+#     print("Both numbers are equal.")
